0af53a50c2ba73ce65bce3223dc81c91c2d2990d
administrator <you@example.com>
2026-07-21 13:17:14 +0530

    Intial commit

---
+++ addition.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a,b;
+cout<<"Enter the number a:";
+cin>>a;
+cout<<"Enter the number b:";
+cin>>b;
+cout<<"the sum is "<<a+b<<endl;
+cout<<"the difference is "<<abs(a-b)<<endl;
+
+cout<<"the product is: "<<a*b<<endl;
+cout<<"the division is: "<<a/b<<endl;
+
+
+return 0;
+}
